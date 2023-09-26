6e6b4973df22406924e13f9315916b7ce2d51d2d
irfan <irfan@me>
2023-09-26 21:37:54 +0530

    First commit to new branch

--- src/com/irfanAK/delta/Delta.java
+++ src/com/irfanAK/delta/Delta.java
@@ -2,6 +2,6 @@ package com.irfanAK.delta;
 
 public class Delta {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Delta V1.00");
     }
 }
